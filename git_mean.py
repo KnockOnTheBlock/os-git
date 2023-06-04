6b5e82637d9203927ebd75ee2ca35a183ebce303
kimnakyeong <skr0596@hufs.ac.kr>
2023-06-04 18:34:53 +0900

    코딩 성적 계산 추가

--- git_mean.py
+++ git_mean.py
@@ -1,10 +1,11 @@
-def mean(kor, math, eng, sci):
-    mean = (kor+math+eng+sci)/4
+def mean(kor, math, eng, sci, code):
+    mean = (kor+math+eng+sci+code)/5
     return round(mean, 1)
 
 kor = int(input('국어 성적: '))
 math = int(input('수학 성적: '))
 eng = int(input('영어 성적: '))
 sci = int(input('과학 성적: '))
+code = int(input('코딩 성적: '))
 
-print(mean(kor, math, eng, sci))
+print(mean(kor, math, eng, sci, code))
