c466b3e60cfa89e99dafc52e3596b2fd1c158d11
kimnakyeong <skr0596@hufs.ac.kr>
2023-06-04 18:15:00 +0900

    adding of science score

--- git_mean.py
+++ git_mean.py
@@ -1,9 +1,10 @@
-def mean(kor, math, eng):
-    mean = (kor+math+eng)/3
+def mean(kor, math, eng, sci):
+    mean = (kor+math+eng+sci)/4
     return round(mean, 1)
 
 kor = int(input('국어 성적: '))
 math = int(input('수학 성적: '))
 eng = int(input('영어 성적: '))
+sci = int(input('과학 성적: '))
 
-print(mean(kor, math, eng))
+print(mean(kor, math, eng, sci))
